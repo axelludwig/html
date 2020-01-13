6af13433ee846f96d8293beb1ad3bb87142624a1
axelludwig <axel.ludwig@outlook.com>
2020-01-13 14:11:18 -0800

    backup

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ var count_mute = 0;
 function pause() {
   if (video.paused) {
     video.play();
-    play_image.src="source/play.png"
+    play_image.src = "source/play.png"
   } else {
     video.pause();
-    play_image.src="source/pause.png"
+    play_image.src = "source/pause.png"
   }
 }
 
@@ -20,9 +20,37 @@ function mute() {
   count_mute++;
   if (0 == count_mute % 2) {
     video.muted = true;
-    mute_image.src="source/mute.png"
+    mute_image.src = "source/mute.png"
   } else {
     video.muted = false;
-    mute_image.src="source/unmute.png"
+    mute_image.src = "source/unmute.png"
   }
+}
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function getWeather() {
+  getLocation();
+  // fetch('https://json.geoiplookup.io/')
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((myJson) => {
+  //     console.log(myJson);
+  //   });
 }
