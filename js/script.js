9a6998b933199868d8ae7f262919937848722f63
axelludwig <axel.ludwig@outlook.com>
2020-02-15 13:12:28 -0800

    fiex the weather

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 var video = document.getElementById("background");
 var music = document.getElementById("background_music");
-// music.volume = 0.3;
 var btn = document.getElementById("myBtn");
 var mute_image = document.getElementById("mute_image");
 var play_image = document.getElementById("play_image");
 var started = false;
 
 var count_mute = 0;
-var count_play = 1;
+var count_play = 0;
 
 var date = document.getElementById("date");
 var time = document.getElementById("time");
+var full_date = document.getElementById("full_date")
 var weather_icon = document.getElementById("iconweather")
-var desc_weather = document.getElementById("descweather");
+var desc_weather1 = document.getElementById("descweather1");
+var desc_weather2 = document.getElementById("descweather2");
+var temp = document.getElementById("temp");
 
 //
 
@@ -25,7 +27,7 @@ function startup() {
 }
 
 video.onended = function () {
-  alert("salut")
+  alert("debug")
   video.load();
   video.play();
 };
@@ -48,7 +50,9 @@ function updateTime() {
 function updateDate() {
   var datef = new Date();
   var res = fixZeros(datef.getUTCDay() + 1) + "/" + fixZeros(datef.getUTCMonth() + 1);
+
   date.innerText = res;
+  full_date.innerText = getFullDate(datef);
 }
 
 function updateIcon(path) {
@@ -57,9 +61,96 @@ function updateIcon(path) {
   weather_icon.src = "http://openweathermap.org/img/wn/" + path + "@2x.png";
 }
 
+function updateTemp(t) {
+  var c = Math.floor(parseInt(t));
+  temp.innerText = c + '°'
+}
+
+function updateDesc(d1, d2) {
+  desc_weather1.innerText = d1;
+  desc_weather2.innerText = d2;
+}
+
 function fixZeros(val) {
   if (val < 10) return "0" + val;
   else return val;
+}
+
+function getFullDate(d) {
+  var res = '';
+
+  switch (d.getDay()) {
+    case 0:
+      res += 'Sunday'
+      break;
+    case 1:
+      res += 'Monday'
+      break;
+    case 2:
+      res += 'Tuesday'
+      break;
+    case 3:
+      res += 'Wednesday'
+      break;
+    case 4:
+      res += 'Thursday'
+      break;
+    case 5:
+      res += 'Friday'
+      break;
+    case 6:
+      res += 'Saturday'
+      break;
+    default:
+      res += '';
+  } res += ' '
+
+  switch (d.getUTCMonth()) {
+    case 0:
+      res += 'January'
+      break;
+    case 1:
+      res += 'February'
+      break;
+    case 2:
+      res += 'March'
+      break;
+    case 3:
+      res += 'April'
+      break;
+    case 4:
+      res += 'May'
+      break;
+    case 5:
+      res += 'June'
+      break;
+    case 6:
+      res += 'July'
+      break;
+    case 7:
+      res += 'August'
+      break;
+    case 8:
+      res += 'September'
+      break;
+    case 9:
+      res += 'October'
+      break;
+    case 10:
+      res += 'November'
+      break;
+    case 11:
+      res += 'December'
+      break;
+    default:
+      res += '';
+  } res += ' '
+
+  res += d.getDay();
+  res += ', ';
+  res += d.getFullYear();
+
+  return res;
 }
 
 //boutons
@@ -98,13 +189,15 @@ function getWeather() {
       var longitude = json.longitude;
       var string = "http://api.openweathermap.org/data/2.5/weather?lat="
         + json.latitude + "&lon=" + json.longitude
-        + "&APPID=e2ec4be1350ccc0e65512e5b66e5c807";
+        + "&APPID=e2ec4be1350ccc0e65512e5b66e5c807&units=metric";
       fetch(string)
         .then((response) => {
           return response.json();
         })
         .then((json) => {
           updateIcon(json.weather[0].icon)
+          updateTemp(json.main.temp)
+          updateDesc(json.weather[0].main, json.weather[0].description)
           console.log(json);
         });
     });
